c2835e3b0a32a773e664d123546f8356a55e7031
atusi <atu729@icloud.com>
2021-01-16 19:15:42 +0900

    first commit

---
+++ app/Models/Blog.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Blog extends Model
+{
+    use HasFactory;
+
+    // テーブル名
+    protected $table = 'blogs';
+
+    // 可変項目
+    protected $fillable = 
+    [
+        'title',
+        'content'
+    ];
+}
